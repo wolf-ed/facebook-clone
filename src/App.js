8809b836d7864ba6bb68a351bb4ef8d3b28ade4b
wolf-ed <wolf7ed@gmail.com>
2022-03-27 16:34:33 +0200

    icons header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="app">
-      <h1>Facebook clone</h1>
       {/* Header */}
       <Header />
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,17 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import HomeIcon from '@mui/icons-material/Home';
+import FlagIcon from '@mui/icons-material/Flag';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import IconButton from '@mui/material/IconButton';
+import AddIcon from '@mui/icons-material/Add';
+import ForumIcon from '@mui/icons-material/Forum';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 
 function Header() {
     return (
@@ -18,11 +29,41 @@ function Header() {
             </div>
             <div className='header__middle'>
                 <div className='header__option'>
-                    <HomeIcon />
+                    <HomeIcon fontSize='large' />
+                </div>
+                <div className='header__option'>
+                    <FlagIcon fontSize='large' />
+                </div>
+                <div className='header__option'>
+                    <SubscriptionsIcon fontSize='large' />
+                </div>
+                <div className='header__option'>
+                    <StorefrontIcon fontSize='large' />
+                </div>
+                <div className='header__option'>
+                    <SupervisedUserCircleIcon fontSize='large' />
                 </div>
             </div>
-            <div className='header__right'></div>
 
+
+
+            <div className='header__right'>
+                <div className='header__info'>
+                    <AccountCircleIcon fontSize='large' />
+                    <h4>Jonny Bravo</h4>
+                </div>
+            </div>
+
+            <IconButton>
+                <AddIcon />
+            </IconButton>
+            <IconButton>
+                <ForumIcon />
+            </IconButton> <IconButton>
+                <NotificationsActiveIcon />
+            </IconButton> <IconButton>
+                <ExpandMoreIcon />
+            </IconButton>
         </div>
     )
 }
